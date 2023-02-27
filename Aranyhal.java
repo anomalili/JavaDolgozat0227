494187b864d0078d4c0c57c9420b929e1ae8ab41
mzsl <95681537+anomalili@users.noreply.github.com>
2023-02-27 09:05:18 +0100

    Add files via upload
    java file létrehozva

---
+++ Aranyhal.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author makra.zs.lili
+ */
+public class Aranyhal {
+
+    private int KIVANSAGOKSZAMA = 3;
+    private int kor;
+    private String kivansagok;
+    private String kifogta;
+
+    public static void Aranyhal(kifogta) {
+        
+
+    }
+
+    public static void Aranyhal(kifogta, kor) {
+
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public void setKifogta(String kifogta) {
+        this.kifogta = kifogta;
+    }
+
+    public static int kivansagokSzama(kifogta) {
+        
+            return int;
+
+    }
+
+    public static String kikkelTalalkozott() {
+        return string[];
+
+    }
+
+    public static boolean kivansagotTeljesit(String kivansag) {
+        return boolean;
+    }
+
+}
